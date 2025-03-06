aaad096ce4d972d96067e9fcac8bddfce8f4bdf5
alexey <alexey.anasov@yandex.by>
2025-03-06 19:38:39 +0300

    сборка gulp + верстка header

--- gulp/tasks/allimg.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-  $.gulp.task('allimg', function () {
-    return $.gulp.src('img/**/*.{png,jpg,svg}')
-        .pipe($.gulp.dest('build/img'));
-  });
-};
--- gulp/tasks/build.js
+++
@@ -1,4 +0,0 @@
-module.exports = function () {
-  $.gulp.task('build', $.gulp.series('clean', 'copy', 'sass', 'scripts', 'css', 'images', 'svg'));
-};
-
